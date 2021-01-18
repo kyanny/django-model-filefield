7ba5da47168a067cb5f634ba6f7f3d0d0531eadc
Kensuke Nagae <kyanny@gmail.com>
2021-01-19 01:35:03 +0900

    add model, migration and admin

--- mysite/uploader/admin.py
+++ mysite/uploader/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Upload
 
 # Register your models here.
+class UploadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+
+admin.site.register(Upload, UploadAdmin)
